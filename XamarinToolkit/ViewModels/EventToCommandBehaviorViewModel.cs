2bb3a0c371fbaa5902a187f0ced827c180c8b0c9
WASIM <wasimalam@virtualemployee.com>
2021-12-29 15:47:18 +0530

    EventToCommandBehavior
    EventToCommandBehavior Code Complete

---
+++ XamarinToolkit/ViewModels/EventToCommandBehaviorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.CommunityToolkit;
+
+namespace XamarinToolkit.ViewModels
+{
+    public class EventToCommandBehaviorViewModel : BaseViewModel
+    {
+        public Command MyCustomCommand { get; }
+        public EventToCommandBehaviorViewModel()
+        {
+            Title = "Event To Command Behavior";
+            MyCustomCommand = new Command(OnMyCustomClicked);
+        }
+
+        private void OnMyCustomClicked(object obj)
+        {
+            ClickCount++;
+        }
+
+        private int _ClickCount;
+        public int ClickCount
+        {
+            get { return _ClickCount; }
+            set { SetProperty(ref _ClickCount, value); }
+        }
+    }
+}
