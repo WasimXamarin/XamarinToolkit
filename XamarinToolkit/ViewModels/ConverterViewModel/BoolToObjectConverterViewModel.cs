e56f232b7d02e552c0bfbdf6d01e3fed2a759c93
WASIM <wasimalam@virtualemployee.com>
2021-12-31 19:09:17 +0530

    Bool To Object Converter
    Bool To Object Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/BoolToObjectConverterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class BoolToObjectConverterViewModel : BaseViewModel
+    {
+        public BoolToObjectConverterViewModel()
+        {
+            Title = "Bool To Object Converter";
+            MyBoolean = true;
+        }
+
+        private bool _MyBoolean;
+        public bool MyBoolean
+        {
+            get { return _MyBoolean; }
+            set { SetProperty(ref _MyBoolean, value); }
+        }
+    }
+}
---
+++ XamarinToolkit/Views/ConverterView/BoolToObjectConverterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ConverterViewModel;
+
+namespace XamarinToolkit.Views.ConverterView
+{
+    public partial class BoolToObjectConverterPage : ContentPage
+    {
+        BoolToObjectConverterViewModel boolToObjectConverterViewModelp;
+        public BoolToObjectConverterPage()
+        {
+            InitializeComponent();
+            BindingContext = boolToObjectConverterViewModelp = new BoolToObjectConverterViewModel();
+        }
+    }
+}
