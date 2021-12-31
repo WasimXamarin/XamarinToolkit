ae31209bdd2467959d7044312906c4457b850fa6
WASIM <wasimalam@virtualemployee.com>
2021-12-31 15:04:59 +0530

    Text Validation Behavior
    Text Validation Behavior Code Complete

---
+++ XamarinToolkit/ViewModels/TextValidationBehaviorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels
+{
+    public class TextValidationBehaviorViewModel : BaseViewModel
+    {
+        public TextValidationBehaviorViewModel()
+        {
+            Title = "Text Validation Behavior";
+        }
+    }
+}
