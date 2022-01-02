29db40f4e1fda7527faeb2d245b207337e372b83
WASIM <wasimalam@virtualemployee.com>
2022-01-02 14:00:11 +0530

    Compare Converter
    Compare Converter Code Complete

---
+++ XamarinToolkit/Views/ConverterView/CompareConverterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ConverterViewModel;
+
+namespace XamarinToolkit.Views.ConverterView
+{
+    public partial class CompareConverterPage : ContentPage
+    {
+        CompareConverterViewModel compareConverterViewModel;
+        public CompareConverterPage()
+        {
+            InitializeComponent();
+            BindingContext = compareConverterViewModel = new CompareConverterViewModel();
+        }
+    }
+}
