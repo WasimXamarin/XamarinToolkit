9fac7802c0e9543fbc676c305944c49e0768879d
WASIM <wasimalam@virtualemployee.com>
2022-01-04 17:09:06 +0530

    Equal Converter
    Equal Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/EnumToBoolConverterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class EnumToBoolConverterViewModel : BaseViewModel
+    {
+       // public string[] AllStates { get; } = Enum.GetNames(typeof(IssueState));
+        public EnumToBoolConverterViewModel()
+        {
+            Title = "Enum To Bool Converter";
+        }
+
+        //IssueState selectedState = IssueState.None;
+        //public IssueState SelectedState
+        //{
+        //    get => selectedState;
+        //    set => SetProperty(ref selectedState, value);
+        //}
+    }
+}
