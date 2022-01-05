65f68c69016f4e7a82d082806c02389b7d855988
WASIM <wasimalam@virtualemployee.com>
2022-01-05 12:19:05 +0530

    Is Null Or Empty Converter
    Is Null Or Empty Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/IsNullOrEmptyConverterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class IsNullOrEmptyConverterViewModel : BaseViewModel
+    {
+        public IsNullOrEmptyConverterViewModel()
+        {
+            Title = "Is Null Or Empty Converter";
+            MyStringValue = null;
+        }
+
+        private string _MyStringValue;
+        public string MyStringValue
+        {
+            get => _MyStringValue;
+            set => SetProperty(ref _MyStringValue, value);
+        }
+    }
+}
