79b1d3436afe5cde75a0a84637a51e8175d7ec83
WASIM <wasimalam@virtualemployee.com>
2022-01-06 13:01:23 +0530

    Item Selected Event Args Converter
    Item Selected Event Args Converter Code Complete

---
+++ XamarinToolkit/Views/ConverterView/ItemSelectedEventArgsConverterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ConverterViewModel;
+
+namespace XamarinToolkit.Views.ConverterView
+{
+    public partial class ItemSelectedEventArgsConverterPage : ContentPage
+    {
+        ItemSelectedEventArgsConverterViewModel itemSelectedEventArgsConverterViewModel;
+        public ItemSelectedEventArgsConverterPage()
+        {
+            InitializeComponent();
+            BindingContext = itemSelectedEventArgsConverterViewModel = new ItemSelectedEventArgsConverterViewModel();
+        }
+    }
+}
