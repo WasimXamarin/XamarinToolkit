b944b20cbe9a49fe3e85bd83c24d9922ce6f1c26
WASIM <wasimalam@virtualemployee.com>
2022-01-07 15:42:07 +0530

    List Is Not Null Or Empty Converter
    List Is Not Null Or Empty Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/ListIsNotNullOrEmptyConverterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class ListIsNotNullOrEmptyConverterViewModel : BaseViewModel
+    {
+        public ListIsNotNullOrEmptyConverterViewModel()
+        {
+            Title = "List Is Not Null Or Empty Converter";
+            MyListValue = "WASIM";
+            //MyListValue = null;
+        }
+
+        private string _MyListValue;
+        public string MyListValue
+        {
+            get => _MyListValue;
+            set => SetProperty(ref _MyListValue, value);
+        }
+    }
+}
---
+++ XamarinToolkit/Views/ConverterView/ListIsNotNullOrEmptyConverterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ConverterViewModel;
+
+namespace XamarinToolkit.Views.ConverterView
+{
+    public partial class ListIsNotNullOrEmptyConverterPage : ContentPage
+    {
+        ListIsNotNullOrEmptyConverterViewModel listIsNotNullOrEmptyConverterViewModel;
+        public ListIsNotNullOrEmptyConverterPage()
+        {
+            InitializeComponent();
+            BindingContext = listIsNotNullOrEmptyConverterViewModel = new ListIsNotNullOrEmptyConverterViewModel();
+        }
+    }
+}
