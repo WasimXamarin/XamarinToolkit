d57256b08c58f921e14e762c22a4b9f4d7dfa499
WASIM <wasimalam@virtualemployee.com>
2022-01-07 16:38:10 +0530

    List To String Converter
    List To String Converter Code Complete

---
+++ XamarinToolkit/Views/ConverterView/ListToStringConverterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ConverterViewModel;
+
+namespace XamarinToolkit.Views.ConverterView
+{
+    public partial class ListToStringConverterPage : ContentPage
+    {
+        ListToStringConverterViewModel listToStringConverterViewModel;
+        public ListToStringConverterPage()
+        {
+            InitializeComponent();
+            BindingContext = listToStringConverterViewModel = new ListToStringConverterViewModel();
+        }
+    }
+}
