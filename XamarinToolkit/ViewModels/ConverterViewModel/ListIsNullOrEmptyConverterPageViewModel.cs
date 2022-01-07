b93c80af864b5acbf9a65ea6451aaf99f2da2a9c
WASIM <wasimalam@virtualemployee.com>
2022-01-07 16:04:46 +0530

    List Is Null Or Empty Converter
    List Is Null Or Empty Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/ListIsNullOrEmptyConverterPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class ListIsNullOrEmptyConverterPageViewModel : BaseViewModel
+    {
+        public ListIsNullOrEmptyConverterPageViewModel()
+        {
+            Title = "List Is Null Or Empty Converter";
+            MyListValue = null;
+            MyListValue = "null";
+        }
+
+        private string _MyListValue;
+        public string MyListValue
+        {
+            get => _MyListValue;
+            set => SetProperty(ref _MyListValue, value);
+        }
+    }
+}
