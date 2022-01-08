892a4f98f32761ffea8eea3f29ebcd5e9157771a
WASIM <wasimalam@virtualemployee.com>
2022-01-09 00:37:00 +0530

    Text Case Converter
    Text Case Converter Code Complete

---
+++ XamarinToolkit/ViewModels/ConverterViewModel/TextCaseConverterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ConverterViewModel
+{
+    public class TextCaseConverterViewModel : BaseViewModel
+    {
+        public TextCaseConverterViewModel()
+        {
+            Title = "Text Case Converter";
+            MyString = "Xamarin forma";
+        }
+
+        private string _EntryValue;
+        public string EntryValue
+        {
+            get => _EntryValue;
+            set => SetProperty(ref _EntryValue, value);
+        }
+
+        private string _MyString;
+        public string MyString
+        {
+            get => _MyString;
+            set => SetProperty(ref _MyString, value);
+        }
+    }
+}
