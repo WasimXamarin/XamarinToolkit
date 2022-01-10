6e0f94de8eae293f59080ba918b775120c0cc702
WASIM <wasimalam@virtualemployee.com>
2022-01-10 12:58:51 +0530

    Translate Extension
    Translate Extension Code Not complete

---
+++ XamarinToolkit/Models/ExtensionsModel/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace XamarinToolkit.Models.ExtensionsModel
+{
+    public readonly struct Language
+    {
+		public Language(string name, string ci)
+		{
+			Name = name;
+			CI = ci;
+		}
+
+		public string Name { get; }
+
+		public string CI { get; }
+	}
+}
---
+++ XamarinToolkit/Views/ExtensionsView/TranslateExtensionPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ExtensionsViewModel;
+
+namespace XamarinToolkit.Views.ExtensionsView
+{
+    public partial class TranslateExtensionPage : ContentPage
+    {
+        TranslateExtensionViewModel translateExtensionViewModel;
+        public TranslateExtensionPage()
+        {
+            InitializeComponent();
+            BindingContext = translateExtensionViewModel = new TranslateExtensionViewModel();
+        }
+
+        async void OnCloseClicked(object? sender, EventArgs e) => await Navigation.PopModalAsync();
+    }
+}
