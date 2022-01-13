33f7e425e5c9ba31274f343d655b0c48232d06ff
WASIM <wasimalam@virtualemployee.com>
2022-01-13 23:28:22 +0530

    Badge View
    Badge View Code Complete

---
+++ XamarinToolkit/Views/ViewsView/BadgeViewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ViewsViewModel;
+
+namespace XamarinToolkit.Views.ViewsView
+{
+    public partial class BadgeViewPage : ContentPage
+    {
+        BadgeViewViewModel badgeViewViewModel;
+        public BadgeViewPage()
+        {
+            InitializeComponent();
+            BindingContext = badgeViewViewModel = new BadgeViewViewModel();
+        }
+    }
+}
