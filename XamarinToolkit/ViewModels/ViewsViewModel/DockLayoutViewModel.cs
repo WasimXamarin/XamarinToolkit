2edef244125a8c60413f855f6c57fc5c71a61c8f
WASIM <wasimalam@virtualemployee.com>
2022-01-14 12:17:50 +0530

    Dock Layout
    Dock Layout Code Complete

---
+++ XamarinToolkit/ViewModels/ViewsViewModel/DockLayoutViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinToolkit.ViewModels.ViewsViewModel
+{
+    public class DockLayoutViewModel : BaseViewModel
+    {
+        public DockLayoutViewModel()
+        {
+            Title = "Dock Layout";
+        }
+    }
+}
