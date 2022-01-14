4ad519c53d29466a03c5d82a51b598891b10e42b
WASIM <wasimalam@virtualemployee.com>
2022-01-14 16:55:33 +0530

    Shield Stacklayout UniformGrid
    Shield Stacklayout UniformGrid Code Complete

---
+++ XamarinToolkit/Views/ViewsView/PopupPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinToolkit.ViewModels.ViewsViewModel;
+
+namespace XamarinToolkit.Views.ViewsView
+{
+    public partial class PopupPage : ContentPage
+    {
+        PopupViewModel popupViewModel;
+        public PopupPage()
+        {
+            InitializeComponent();
+            BindingContext = popupViewModel = new PopupViewModel();
+        }
+    }
+}
